a7c6dfb2ace0e251ea876bc4c6492c2466c54319
Fabio Montezuma <fabio.montezuma@soaexpert.com.br>
2019-07-25 16:49:48 -0300

    Add HTTPS Port on NLB

--- 09-nlb.tf
+++ 09-nlb.tf
@@ -17,6 +17,17 @@ resource "aws_lb_listener" "nlb_listener" {
   load_balancer_arn = "${aws_lb.nlb.arn}"
   port              = "80"
   protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.nlb_tg.arn}"
+  }
+}
+
+resource "aws_lb_listener" "nlb_listener" {
+  load_balancer_arn = "${aws_lb.nlb.arn}"
+  port              = "443"
+  protocol          = "TCP"
   #ssl_policy        = "ELBSecurityPolicy-2016-08"
   #certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
 
